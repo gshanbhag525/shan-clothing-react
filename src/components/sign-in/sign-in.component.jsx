3fb4f4c72027065fbcaaa7ecd191e1b44cfdd211
gshanbhag525 <17478096+gshanbhag525@users.noreply.github.com>
2021-08-21 12:42:18 +0530

    fixed sign up component level as same as sign in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -49,21 +49,22 @@ class SignIn extends React.Component {
                         name='email' type='email'
                         value={this.state.email } 
                         handleChange={this.handleChange}
-                        label='email'
+                        label='Email'
                         required />
 
                     <FormInput 
                         name='password' type='password'
                         value={this.state.password } 
                         handleChange={this.handleChange}
-                        label='password'
+                        label='Password'
                     required />
 
                     <div className="buttons">
                         <CustomButton type='submit'>
                             SIGN IN
                         </CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        <CustomButton type='button'
+                            onClick={signInWithGoogle} isGoogleSignIn>
                             Sign in with Google
                         </CustomButton>
                     </div>
