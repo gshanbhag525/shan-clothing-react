934711385b766638a71dbf698499bd4e6ed27fb3
gshanbhag525 <17478096+gshanbhag525@users.noreply.github.com>
2021-08-21 11:25:41 +0530

    implemented firebase utils , included ability tostore authentiated users into firebase databse

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from  './pages/shop/shop.component';
+import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument, firestore } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
     super();
 
-    this.state  = {
+    this.state = {
       currentUser: null
     }
   }
@@ -19,10 +19,24 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
+
     });
   }
 
@@ -31,7 +45,7 @@ class App extends React.Component {
   }
 
   render() {
-    return(
+    return (
       <div>
         <Header currentUser={this.state.currentUser} />
         <Switch>
@@ -41,7 +55,7 @@ class App extends React.Component {
         </Switch>
       </div>
     );
-  }  
+  }
 }
 
 export default App;
