c718ada2c0c5eae370e6e879f18a8137b9e3f506
gshanbhag525 <17478096+gshanbhag525@users.noreply.github.com>
2021-08-22 13:08:37 +0530

    added redux, redux-logger, react-redux 1.added provider in indexjs and then root reducer->user reducer->storejs middleware and user actions

--- src/index.js
+++ src/index.js
@@ -6,24 +6,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App /> 
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App /> 
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-
-// uncomment when needed
-// serviceWorkerRegistration.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
