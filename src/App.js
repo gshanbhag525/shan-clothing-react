68eab3973cc61621ba225eb4bfc5ff60bbb65482
gshanbhag525 <17478096+gshanbhag525@users.noreply.github.com>
2021-08-23 21:04:19 +0530

    refactored code to use createStructuresSelector  selector

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentUser } from "./redux/user/user.selectors";
+import { createStructuredSelector } from "reselect";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -45,7 +47,7 @@ class App extends React.Component {
           <Route
             exact
             path="/signin"
-            render={() => 
+            render={() =>
               this.props.currentUser ? (
                 <Redirect to="/" />
               ) : (
@@ -59,8 +61,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
