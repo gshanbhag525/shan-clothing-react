108b0777e256665032db30ec5aa382f11d6ec5d4
gshanbhag525 <17478096+gshanbhag525@users.noreply.github.com>
2021-08-24 17:16:19 +0530

    added subroute to shop page to get dynamic category name

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
+import CategoryPage from "../category/category.component";
+
 import CollectionOverview from "../../components/collection-overview/collection-overview.component";
 
-const ShopPage = ({ match }) => {
-  console.log(match);
-  return (
-    <div className="shop-page">
-      <Route exact path={`${match.path}`} component={CollectionOverview} />
-    </div>
-  );
-};
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
+    <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+  </div>
+);
 
 export default ShopPage;
